e63411d03f603106ae249893823095b39f4d8877
richard.klees@rwth-aachen.de <richard.klees@rwth-aachen.de>
2016-01-19 10:55:34 +0100

    find ancestors of classes

--- method_signatures.py
+++ method_signatures.py
@@ -14,7 +14,8 @@ def class_files(path):
                 if class_file_re.match(_f):
                     yield os.path.join(path, f)
 
-class_info_re = re.compile("^\s*(abstract)?\s*class\s+(\w+)\s+(.*)$", re.MULTILINE)
+class_re = re.compile("^\s*(abstract|final)?\s*class\s+(\w+)(\s|/)+(.*)$", re.MULTILINE)
+class_info_re = re.compile(".*extends\s+(\w+)")
 
 def class_file_contents(path):
     for cf in class_files(path):
@@ -23,21 +24,36 @@ def class_file_contents(path):
 
 def class_infos(path):
     for (path, c) in class_file_contents(path):
-        res = class_info_re.search(c)
+        res = class_re.search(c)
         if not res:
             print "ERROR in %s" % path
             continue
-        yield ClassInfo(path, res.group(2), res.group(3)) 
+        yield ClassInfo(path, res.group(2), res.group(4))
+
+def ancestor_name(info):
+    res = class_info_re.match(info)
+    if res is None:
+        return None
+    return res.group(1)
 
 class ClassInfo(object):
     def __init__(self, path, name, info):
         self.path = path
         self.name = name
+        self.ancestor_name = ancestor_name(info)
 
     def pp(self):
-        return "%s\n%s" % (self.path, self.name)
+        return "%s\n%s\n\t%s" % (self.path, self.name, self.ancestor_name)
+
+CLASSES = {}
+
+def make_ILIAS_CLASSES():
+    for c in class_infos("/home/lechimp/Code/ILIAS"):
+        CLASSES[c.name] = c
 
 if __name__ == "__main__":
+    make_ILIAS_CLASSES()
     for c in class_infos("/home/lechimp/Code/ILIAS"):
+        CLASSES[c.name] = c
         print "-------------------------------------------"
         print c.pp()
