26bcbc7f9166ca51e989a2aa6b2013a6aef01406
richard.klees@rwth-aachen.de <richard.klees@rwth-aachen.de>
2016-01-19 10:18:41 +0100

    find class info

--- method_signatures.py
+++ method_signatures.py
@@ -13,3 +13,31 @@ def class_files(path):
             else:
                 if class_file_re.match(_f):
                     yield os.path.join(path, f)
+
+class_info_re = re.compile("^\s*(abstract)?\s*class\s+(\w+)\s+(.*)$", re.MULTILINE)
+
+def class_file_contents(path):
+    for cf in class_files(path):
+        with open(cf) as f:
+            yield (cf, f.read())
+
+def class_infos(path):
+    for (path, c) in class_file_contents(path):
+        res = class_info_re.search(c)
+        if not res:
+            print "ERROR in %s" % path
+            continue
+        yield ClassInfo(path, res.group(2), res.group(3)) 
+
+class ClassInfo(object):
+    def __init__(self, path, name, info):
+        self.path = path
+        self.name = name
+
+    def pp(self):
+        return "%s\n%s" % (self.path, self.name)
+
+if __name__ == "__main__":
+    for c in class_infos("/home/lechimp/Code/ILIAS"):
+        print "-------------------------------------------"
+        print c.pp()
