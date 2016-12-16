95bc8128bb5a05d42a3026f0ca0ef94f7d107f34
Alex Killing <alex.killing@gmx.de>
2016-12-16 13:46:40 +0100

    legacy lang: some fixes

--- legacy_lang_vars/legacy_lang_vars.php
+++ legacy_lang_vars/legacy_lang_vars.php
@@ -9,190 +9,6 @@
 
 namespace il\Language;
 
-/**
- * Class ClassInfo
- * @package il\Language
- * @author  Alex Killing <killing@leifos.de>
- * Based heavily on code from mjansen@databay.de
- */
-class ClassInfo
-{
-	/**
-	 * @var string
-	 */
-	protected $path = '';
-
-	/**
-	 * @var string
-	 */
-	protected $class = '';
-
-	/**
-	 * @var array
-	 */
-	protected $lang_vars = array();
-
-	/**
-	 * ClassInfo constructor.
-	 * @param $path    string
-	 * @param $class   string
-	 */
-	public function __construct($path, $class)
-	{
-		$this->path  = $path;
-		$this->class = $class;
-	}
-
-	/**
-	 * @param string $lang_var
-	 */
-	public function pushLangVar($lang_var)
-	{
-		if (!in_array($lang_var, $this->lang_vars))
-		{
-			$this->lang_vars[] = $lang_var;
-		}
-	}
-
-	/**
-	 * @return \Generator
-	 */
-	public function usedLangVars($a_candidates)
-	{
-		foreach ($this->lang_vars as $v)
-		{
-			if (isset($a_candidates[$v]))
-			{
-				yield $v;
-			}
-		}
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getPath()
-	{
-		return $this->path;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getClass()
-	{
-		return $this->class;
-	}
-
-	/**
-	 * @param string $file_path
-	 * @return \Generator
-	 */
-	public static function parseClassInfosFromFile($file_path)
-	{
-		$tokens     = token_get_all(file_get_contents($file_path));
-		/*$transl = array_map(function($e) {
-			return array(token_name($e[0]), $e[1], $e[2]);
-		}, $tokens);
-		
-		var_dump($transl); exit;*/
-
-		$num_tokens = count($tokens);
-
-		/** @var $last_class self */
-		$last_class = null;
-		$namespace  = '';
-
-		for ($i = 0; $i < $num_tokens; $i++) {
-			if (is_string($tokens[$i])) {
-				continue;
-			}
-
-			$token = $tokens[$i][0];
-			$line  = $tokens[$i][2];
-			switch ($token) {
-				case T_NAMESPACE:
-					$namespace = self::getNamespaceName($tokens, $i);
-					break;
-
-				case T_CLASS:
-					if ($last_class) {
-						yield $last_class;
-					}
-
-					$class_name = self::getClassName($namespace, $tokens, $i);
-					$last_class = new self($file_path, $class_name);
-					break;
-
-				case T_STRING:
-					if ($tokens[$i][1] == "txt" && $tokens[$i-1][0] == T_OBJECT_OPERATOR)
-					{
-						$lv = str_replace(array("'", '"'), "", $tokens[$i+2][1]);
-						if ($lv != "" && $last_class) {
-							$last_class->pushLangVar($lv);
-						}
-					}
-					break;
-			}
-		}
-
-		if ($last_class) {
-			yield $last_class;
-		}
-	}
-
-	/**
-	 * @param array $tokens
-	 * @param int   $i
-	 * @return bool|string
-	 */
-	protected static function getNamespaceName(array $tokens, $i)
-	{
-		if (isset($tokens[$i + 2][1])) {
-			$namespace = $tokens[$i + 2][1];
-
-			for ($j = $i + 3; ; $j += 2) {
-				if (isset($tokens[$j]) && $tokens[$j][0] == T_NS_SEPARATOR) {
-					$namespace .= '\\' . $tokens[$j + 1][1];
-				} else {
-					break;
-				}
-			}
-
-			return $namespace;
-		}
-
-		return false;
-	}
-
-	/**
-	 * @param string $namespace
-	 * @param array  $tokens
-	 * @param int    $i
-	 * @return string
-	 */
-	protected static function getClassName($namespace, array $tokens, $i)
-	{
-		$i += 2;
-		$namespaced = false;
-		$class_name = $tokens[$i][1];
-
-		if ($class_name === '\\') {
-			$namespaced = true;
-		}
-
-		while (is_array($tokens[$i + 1]) && $tokens[$i + 1][0] !== T_WHITESPACE) {
-			$class_name .= $tokens[++$i][1];
-		}
-
-		if (!$namespaced && $namespace) {
-			$class_name = $namespace . '\\' . $class_name;
-		}
-
-		return $class_name;
-	}
-}
-
 
 /**
  * Class LegacyLangVars
@@ -236,11 +52,8 @@ class LegacyLangVars
 		$t0 = microtime(true);
 		try {
 			$this->readCandidates($args[2]);
-			foreach ($this->getIliasClasses($args[1]) as $class_info) {
-				/** @var $class_info ClassInfo */
-				foreach ($class_info->usedLangVars($this->candidates) as $lv) {
-					unset($this->candidates[$lv]);
-				}
+			foreach ($this->getIliasClassFiles($args[1]) as $file) {
+				$this->searchLangVars($file->getPathname());
 			}
 		} catch (\Exception $e) {
 			echo $e->getMessage() . "\n";
@@ -260,19 +73,48 @@ class LegacyLangVars
 	}
 
 	/**
-	 * @param $path string
+	 * @param string $file_path
 	 * @return \Generator
 	 */
-	protected function getIliasClasses($path)
+	public function searchLangVars($file_path)
 	{
-		foreach ($this->getIliasClassFiles($path) as $file) {
-			/** @var $file \SplFileInfo */
-			foreach (ClassInfo::parseClassInfosFromFile($file->getPathname()) as $class_info) {
-				/** @var $class ClassInfo */
-				yield $class_info;
+		$tokens     = token_get_all(file_get_contents($file_path));
+
+		/*if (is_int(strpos($file_path, "TrackingItemsTableGUI")))
+		{
+			$transl = array_map(function($e) {
+				return array(token_name($e[0]), $e[1], $e[2]);
+			}, $tokens);
+
+			var_dump($transl); exit;
+		}*/
+
+
+		$num_tokens = count($tokens);
+
+		for ($i = 0; $i < $num_tokens; $i++) {
+			if (is_string($tokens[$i])) {
+				continue;
+			}
+
+			$token = $tokens[$i][0];
+			switch ($token) {
+
+				case T_STRING:
+				case T_CONSTANT_ENCAPSED_STRING:
+					//if ($tokens[$i][1] == "txt" && $tokens[$i-1][0] == T_OBJECT_OPERATOR)
+					if (true)
+					{
+						$lv = str_replace(array("'", '"'), "", $tokens[$i][1]);
+						if ($lv != "") {
+							unset($this->candidates[$lv]);
+						}
+					}
+					break;
 			}
 		}
 	}
+
 
 	/**
 	 * @param $path string
